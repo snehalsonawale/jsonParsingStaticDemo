e2e5f8e76f0420ff873cf31d58d4679ce60be8bf
snehalsonawale222 <snehal.sonawale@firstcry.com>
2020-04-12 00:54:37 +0530

    cheacking

--- app/src/main/java/com/example/jsonparsingstaticdemo/MainActivity.java
+++ app/src/main/java/com/example/jsonparsingstaticdemo/MainActivity.java
@@ -127,6 +127,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
             String namearray= jsonArray.getString(Integer.parseInt("name"));
            String idarray= jsonArray.getString(Integer.parseInt("id"));
 
